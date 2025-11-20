549c1b56bc5f698273de895ac54db66ac715b5af
Dahisee <chuksgabriella2019@gmail.com>
2025-11-20 21:42:02 +0000

    feat: add Card component and integrate it into Home page

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/layout/Header";
+import Card from "@/components/common/Card"
 export default function Home() {
   return (
     <div>
@@ -6,6 +7,8 @@ export default function Home() {
       <main className="p-6">
         <h1 className="text-2xl font-bold">Home Page</h1>
         <p>This is the home page content.</p>
+        <Card title="First Card" content="This is the content of the first card"/>
+        <Card title="Second Card" content="This is the second card content"/>
       </main>
     </div>
   )
